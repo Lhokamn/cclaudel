6e54bbc24ca50c47bc7b3c9a8c054b28d7066cc9
lhokam <lhokam.claudel@gmail.com>
2024-01-14 18:14:00 +0100

    reprise du projet en mode nextJS

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  
+  return (
+    <main>
+      <div>
+        <div >
+          <h1>Portfolio - Corentin CLAUDEL</h1>
+        </div>
+      </div>
+    </main>
+  )
+}
