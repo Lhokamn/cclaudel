7ca11e285fe4355084c07339bcec69c26c251851
lhokam <lhokam.claudel@gmail.com>
2024-01-21 23:03:57 +0100

    Rajout de petite modification

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,21 @@ export default function Home() {
         <div className={styles.header}>
           <h1>Corentin CLAUDEL</h1>
         </div>
-        <div className={styles.aboutme}>
+        <div className={styles.fullContent}>
           <Image 
             src="/img/pp.jpeg"
             width={300}
             height={300}
             alt="picture of me"
             />
-          <h2>About Me</h2>
-          <p>Je suis un étudiant en Informatique à l'IUT Informatique de Lyon 1 la doua. Mais je suis Apprenti Technicien à l'entreprise <a href="https://endrix.com">Endrix</a> dans un but de professionnalisation de mon parcours professionnel </p>
-          <ContactSection contacts={contact}></ContactSection>
+          <div className={styles.myJob}>
+            <p>Administrateur Système et Réseau</p>
+          </div>
+          <div className={styles.aboutme}>
+            <h2>About Me</h2>
+            <p>Je suis un étudiant en Informatique à l'IUT Informatique de Lyon 1 la doua. Mais je suis Apprenti Technicien à l'entreprise <a href="https://endrix.com">Endrix</a> dans un but de professionnalisation de mon parcours professionnel </p>
+            <ContactSection contacts={contact}></ContactSection>
+          </div>
         </div>
       </div>
     </main>
