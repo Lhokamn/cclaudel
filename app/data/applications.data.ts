525db55cffa3f3d8efddd3ce812f0d72bc565918
lhokam <lhokam.claudel@gmail.com>
2024-07-07 11:46:00 +0200

    Modification du fichier d'application

---
+++ app/data/applications.data.ts
@@ -0,0 +1,44 @@
+import { ApplicationsInformationType } from '@/app/types/applications.type'
+
+export const apps:ApplicationsInformationType[] = [
+    {
+        nomApp: 'Documentation',
+        url: 'https://doc.cclaudel.fr',
+        description:'Documentation Opensource en français'
+    },
+/**      {
+        nomApp: 'Passwd',
+        url: 'https://passwd.cclaudel.fr',
+        description:'Partage de mot de passe de manière sécurisés'
+    },*/
+    {
+        nomApp: 'VaultWarden',
+        url: 'https://vault.cclaudel.fr',
+        description:'Stockage de mot de passe héberger chez moi'
+    },
+    {
+        nomApp: 'Stardust-Programmers',
+        url:'https://stardust-Programmers.cclaudel.fr',
+        description:'Site Web développé pour la nuit de l\'informatique 2023 en Collaboration'
+    },
+    {
+        nomApp: 'Mon CV',
+        url: 'https://cclaudel.fr',
+        description: 'Portfolio en ligne réalisés avec NextJS'
+    },
+    {
+        nomApp: 'Mail Serveur',
+        url: 'mail.cclaudel.fr',
+        description: 'Création d\'un serveur de mail pour le nom de dommaine cclaudel.fr'
+    },
+    {
+        nomApp: 'Nextcloud',
+        url: 'https://cloud.cclaudel.fr',
+        description: 'Instance privé de Nextcloud pour stockage externe de fichier'
+    },
+    {
+        nomApp: 'SearXNG',
+        url: 'https://search.cclaudel.fr',
+        description: 'Instance privé du moteur de recherche searxng'
+    }
+]
